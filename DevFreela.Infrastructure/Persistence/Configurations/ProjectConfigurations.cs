b5e6556a47cb91a82647f3afa245016730aa956a
slsiam <silas.stos.ccc@gmail.com>
2023-04-18 23:12:34 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.Infrastructure/Persistence/Configurations/ProjectConfigurations.cs
@@ -0,0 +1,35 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class ProjectConfigurations : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder
+                 .ToTable("Projects")
+                .HasKey(project => project.Id);
+
+            builder
+             .Property(p => p.TotalCost)
+                 .HasColumnType("decimal(18,4)");
+
+            builder
+                .HasOne(p => p.Freelancer)
+                    .WithMany(f => f.FreelanceProjects)
+                    .HasForeignKey(p => p.IdFreelancer)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                 .HasOne(p => p.Client)
+                    .WithMany(c => c.OwnedProjects)
+                    .HasForeignKey(p => p.IdClient)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ DevFreela.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+
+        public UserRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> CreateUserAsync(User user)
+        {
+
+            await _dbContext.Users.AddAsync(user);
+
+            _dbContext.SaveChanges();
+
+            return user.Id;
+        }
+
+        public Task<User> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        //public Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash)
+        //{
+        //  //  return _dbContext.Users.SingleOrDefaultAsync(user => user.Email == email && user.Password == passwordHash);
+        //}
+
+        public async Task<User> GetUserById(int id)
+        {
+            return await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == id);
+        }
+    }
+}
